aa9f695971c18ef6a75133492d39e17572653912
Conner Gleason <connereg@optonline.net>
2022-08-15 14:32:58 -0400

    Completed Lab

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+const numberInput = document.getElementById("spacesNumber") as HTMLInputElement;
+const stringInput = document.getElementById("stringToPrepend") as HTMLInputElement;
+const prependButton = document.getElementById("prependButton") as HTMLButtonElement;
+
+console.log(prependButton)
 // your code here
+prependButton!.addEventListener("click", function () {
+    console.log("Trigger working...")
+    let prependResult = prepend(parseInt(numberInput!.value), stringInput!.value)
+    console.log(prependResult)
+
+});
+
+
+
+
+function prepend(numSpaces: number, inputString: string) {
+    let spacesString: String = ""
+    for (let i=0; i < numSpaces; i++) {
+        spacesString = spacesString + " "
+    }
+    let resultString: String = spacesString + inputString
+
+    return resultString
+}
